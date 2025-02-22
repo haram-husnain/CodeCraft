f6ac4be87e26a974e834c1906449dbdb827b1141
Haram Husnain <hhusnain@mymail.mapua.edu.ph>
2025-02-22 19:55:37 +0800

    root api url

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from accounts.views import RegisterView, LoginView
+from accounts.views import RegisterView, LoginView, APIRootView
 
 urlpatterns = [
+    path('', APIRootView.as_view(), name='api-root'),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,6 +2,9 @@ from rest_framework import generics, permissions
 from django.contrib.auth import get_user_model
 from .serializers import UserSerializer
 
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
 # Create your views here.
 User = get_user_model()
 
@@ -11,3 +14,14 @@ class RegisterView(generics.CreateAPIView):
 
 class LoginView(generics.CreateAPIView):
     pass
+
+#root api/ url
+class APIRootView(APIView):
+    def get(self, request):
+        return Response({
+            'message': 'Welcome to the CodeCraft API!',
+            'endpoints': {
+                'register': '/api/register/',
+                'login': '/api/login/',
+            }
+        })
