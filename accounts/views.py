ee363f8ced0bc290104d5b4d480b1eb76a130be3
Haram Husnain <hhusnain@mymail.mapua.edu.ph>
2025-02-22 20:07:25 +0800

    loginview access token added

--- accounts/views.py
+++ accounts/views.py
@@ -5,6 +5,10 @@ from .serializers import UserSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+from rest_framework import status
+
 # Create your views here.
 User = get_user_model()
 
@@ -13,7 +17,24 @@ class RegisterView(generics.CreateAPIView):
     serializer_class = UserSerializer
 
 class LoginView(generics.CreateAPIView):
-    pass
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        # Authenticate the user
+        user = authenticate(username=username, password=password)
+        if user:
+            # Generate tokens
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'message': 'Login successful',
+                'tokens': {
+                    'refresh': str(refresh),
+                    'access': str(refresh.access_token),
+                }
+            }, status=status.HTTP_200_OK)
+        else:
+            return Response({'message': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
 
 #root api/ url
 class APIRootView(APIView):
